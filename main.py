7092537988023113b1bb9f69791c83485a6e45f9
benedek99 <47915637+benedek99@users.noreply.github.com>
2021-10-20 12:50:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,111 +8,36 @@ def hash_func(str):
 
 
 if __name__ == '__main__':
-    """
-    string="pythonpool.com"
-    encoded=string.encode()
-    result = hashlib.sha512(encoded)
-    print("String : ", end ="")
-    print(string)
-    print("Hash Value : ", end ="")
-    print(result)
-    print("Hexadecimal equivalent: ",result.hexdigest())
-    print("Digest Size : ", end ="")
-    print(result.digest_size)
-    print("Block Size : ", end ="")
-    print(result.block_size)
-
-    pubkey, privkey = rsa.newkeys(1024)
-    message = "xyz".encode('utf-8')
-
-    hash = rsa.compute_hash(encoded, 'SHA-512')
-    print(hash)
-    print(hash_func(string))
-
-    signature = rsa.sign(message, privkey, 'SHA-512')
-    print(signature)
-    signature = rsa.sign_hash(hash, privkey, 'SHA-512')
-    print(signature)
-
-    #b1 = Block()
-
-    #print(mine_block(b1))
 
     a = User("A")
     b = User("B")
+    c = User("C")
+    d = User("D")
 
     t1 = Transaction(a, b, 10)
-
-   # print(verify_transaction(t1,t1.sender))
-
-    blockchain = [Block(0, 0, 0)]
-
-    for i in range(0):
-        print(mine_block(blockchain[i]))
- 
-
-    a = User("A")
-    b = User("B")
-    #c = User("C")
-    #d = User("D")
-
-    t1 = Transaction(a,b,10)
-    print(t1.verify())
-
-    t2 = Transaction(a,b,100)
-    print(t1.verify())
-
-
-    (pubkey, privkey) = rsa.newkeys(1024)
-    message = 'Go left at the blue tree'.encode()
-    hash = rsa.compute_hash(message, 'SHA-512')
-    print(hash)
-    signature = rsa.sign_hash(hash, privkey, 'SHA-512')
-    print(signature)
-    signature = rsa.sign(message, privkey, 'SHA-512')
-    print(signature)
-
-    message2 = 'Go left at the blue tree'.encode()
-    print(rsa.verify(message2, signature, pubkey) == 'SHA-512')
-
-    a = User("A")
-    b = User("B")
-    #c = User("C")
-    #d = User("D")
-
-    t1 = Transaction(a, b, 10)
-    t2 = Transaction(a, b, 10)
-    t3 = Transaction(a, b, 10)
-    t4 = Transaction(a, b, 10)
-
-    transactions = [t1,t2,t3,t4]
-    print(transactions)
-    print(build_merkle_tree(transactions, 4))
-    print(transactions)
-"""
-
-    a = User("A")
-    b = User("B")
-
-    t1 = Transaction(a, b, 10)
-    t2 = Transaction(a, b, 10)
-    t3 = Transaction(a, b, 10)
+    t2 = Transaction(a, c, 10)
+    t3 = Transaction(b, d, 20)
     t4 = Transaction(a, b, 100)
-    t5 = Transaction(a, b, 10)
+    t5 = Transaction(b, a, 10)
+    t6 = Transaction(c, d, 20)
+    t7 = Transaction(c, a, 30)
+    t8 = Transaction(d, b, 200)
+    t9 = Transaction(b, c, 10)
+    t10 = Transaction(a, d, 20)
+    t11 = Transaction(d, a, 10)
 
-    ledger = [t1, t2, t3, t4, t5, t1, t2, t3, t4, t5, t5, t5]
+    ledger = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11]
 
     bc = Blockchain()
-    #bc.add_block(ledger, 4)
+    # bc.add_block(ledger, 4)
     bc.build(ledger, 4)
     print(a.get_balance())
     print(b.get_balance())
 
-    ledger.append(t5)
-    ledger.append(t5)
+    ledger.append(Transaction(b, a, 20))
+    ledger.append(Transaction(b, a, 20))
+    ledger.append(Transaction(b, a, 20))
     bc.build(ledger, 4)
-    bc.print()
+
     bc.chain[1].print()
-
-
-
+    bc.print()
